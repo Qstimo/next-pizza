83fbd7ec1ad2616a4ea0d5359ac87bed8aa5b790
Vadim Tolpekin <wadt97@gmail.com>
2024-07-27 20:16:27 +0300

    first commit

---
+++ components/shared/index.ts
@@ -0,0 +1,11 @@
+export {Categories} from './categories';
+export {Title} from './title';
+export { Header } from './header';
+export { Container } from './container';
+export { SortPopup } from './sort-popup';
+export { TopBar } from './top-bar';
+export { Filters } from './filters';
+export { FilterCheckbox } from './filter-checkbox';
+
+
+
